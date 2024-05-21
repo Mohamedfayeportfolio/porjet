65e7288911725ebf725afef98be96b8762620233
FAYE MOUHAMED <159127634+Mohamedfayeportfolio@users.noreply.github.com>
2024-05-21 16:54:16 +0200

    Add files via upload

---
+++ lift/git --version.ps1
@@ -0,0 +1,11 @@
+git --version
+git config --global user.name "mouhamed faye"
+git config --global user.email "fayemouha67@example.com"
+cd C:\Users\lenovo\Documents\lift
+git init
+git commit -m "Initial commit with project files and images"
+git remote add origin https://github.com/votre_nom_utilisateur/votre_depot.git
+git branch -M main
+git push -u origin main
+
+
